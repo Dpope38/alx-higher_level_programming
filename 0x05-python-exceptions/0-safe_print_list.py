8a91addf44be816ef47c75147d80358e3a71fdce
popeopzy <popeopzy@gmail.com>
2023-12-19 04:09:19 +0100

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        while True:
+            print(my_list[count], end=" ")
+            count += 1
+            if count == x:
+                break
+        except indexError:
+            pass
+        finally:
+            print()
+        return count
