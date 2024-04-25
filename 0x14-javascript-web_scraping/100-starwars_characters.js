d59b004ddd629c298557c909ac8763fbd1a47598
popeopzy <popeopzy@gmail.com>
2024-04-25 03:38:26 +0100

    modify

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 const req = require('request');
-const id = process.argv[1];
+const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
 req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
   }
-    const data = JSON.parse(body);
+  const data = JSON.parse(body);
   const dd = data.characters;
   for (const i of dd) {
     req.get(i, function (error, res, body1) {
