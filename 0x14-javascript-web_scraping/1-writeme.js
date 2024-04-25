d90ac1454c9dbf050cf5f71e6b9671f37f39085e
popeopzy <popeopzy@gmail.com>
2024-04-25 03:48:16 +0100

    modify

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf-8', (err, data) => {
-    if (err || !(filePath && content)) {
+    if (err) {
         console.log(err)
     }
 })
