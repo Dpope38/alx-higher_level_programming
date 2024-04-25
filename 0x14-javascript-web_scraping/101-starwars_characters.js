7925def5f8a91231ed4be30cdc1197444e848abb
popeopzy <popeopzy@gmail.com>
2024-04-25 03:44:06 +0100

    Modify

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+    printChars(characters, 0);
   }
 });
 
